98419cd6209dd74f1fe9ac72f8a99f4b9e5ba694
code.labsquire <code@labsquire.com>
2023-07-13 12:38:53 +0530

    file upload

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-
 if (!process.env.MONGO_URL) {
   throw new Error("Please add the MONGO_URL environment variable");
 }
---
+++ src/services/s3DataServiceProvider.ts
@@ -0,0 +1,42 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import * as AWS from "aws-sdk";
+
+export class S3DataServiceProvider {
+    s3Service: AWS.S3;
+    constructor() {
+        this.s3Service = new AWS.S3({
+            accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+            region: process.env.AWS_S3_BUCKET_REGION,
+            signatureVersion: 'v4'
+        });
+    }
+    async getPreSignedUrl(
+        fileName,
+        type = "put",
+        slug?,
+        expireSeconds = parseInt(process.env.PRE_SIGNED_URL_EXPIRES, 10),
+        bucket = process.env.AWS_S3_BUCKET
+    ) {
+
+        let key = "";
+        if (slug) {
+            key += `${slug}/`;
+        }
+
+        key += fileName;
+        const params = {
+            Bucket: bucket,
+            Expires: expireSeconds,
+            Key: key,
+        };
+
+        if (type == "put") {
+            params["ContentType"] = "application/x-www-form-urlencoded";
+        }
+
+        const method = type === "put" ? "putObject" : "getObject";
+        return await this.s3Service.getSignedUrl(method, params);
+    }
+}
